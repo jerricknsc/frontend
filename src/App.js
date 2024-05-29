2c8909efc17b7676db498472a9ab69469a008255
jerricknsc <jerricknsc@gmail.com>
2024-05-30 01:11:43 +0800

    install github pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 // src/App.js
 import React from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
+// import { Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Login from './Login';
 import Home from './Home';
 
 function App() {
   return (
     <div className="App">
-      <Link> Login </Link>
+      <nav>
+        <Link to="./Login"> Login </Link> <br></br>
+        <Link to="/">Home</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,12 @@
+// src/Home.js
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h2>Home Page</h2>
+      <p>Welcome to the Home Page!</p>
+    </div>
+  );
+}
+export default Home;
